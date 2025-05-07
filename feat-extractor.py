13aa360e1b91a530ff3b2a09748a38138d1ee96b
jaclin <jaclin@umich.edu>
2025-05-07 13:53:58 -0400

    keypts and descriptors

---
+++ feat-extractor.py
@@ -0,0 +1,24 @@
+"""Feature extractor for aerial mapping."""
+import os
+import cv2
+
+class FeatureExtractor:
+    """
+    A class for extracting the feature for an image.
+    Computes the keypoints (corners) and descriptors
+    for each image.
+    """
+    
+    def __init__(self, temp_dir=None, output_dir, nfeats=5000):
+        """Handle the initilization of the feature extractor."""
+        if not temp_dir:
+            self.temp_dir = os.path.join(os.getcwd(), "temp")
+        else:
+            self.temp_dir = temp_dir
+        self.output_dir = output_dir
+    
+    def detect_and_describe(self, image):
+        """Extract the key pts and the descriptor from an image.""""
+        descriptor = cv2.ORB_create(nfeatures=nfeats)
+        kp, des = descriptor.detectAndCompute(image, None)
+        pts = cv2.KeyPoint_convert(kp)
