b167bf4eed4e81f8e75999366e83e4dacc709ee0
jaclin <jaclin@umich.edu>
2025-05-07 01:24:17 -0400

    style fix

--- image_processor.py
+++ image_processor.py
@@ -1,48 +1,77 @@
-import cv2
+# pylint: disable=no-member,unpacking-non-sequence,too-few-public-methods
+"""Preprocessor class."""
 import os
+import cv2
 import numpy as np
 
 
 class ImageProcessor:
-    def __init__(self, images_dir=os.path.join(os.getcwd(), "images/test"),
-                focal_length=4800, principal_x=2254, principal_y=2048):
-        self.focal_length = focal_length
-        self.principal_x = principal_x
-        self.principal_y = principal_y
-        self.mtx = np.array([
-            [self.focal_length, 0, self.principal_x],
-            [0, self.focal_length, self.principal_y],
-            [0, 0, 1]
-        ])
-        self.distortion_coeff = np.zeros(5)
+    """A class for preprocessing the photos."""
 
-        self.images_dir = images_dir
+    def __init__(
+        self,
+        images_dir=None,
+        focal_length=4800,
+        principal_x=2254,
+        principal_y=2048
+    ):
+        """Handle the initialization of the image processor class."""
+        self.camera = {
+            "mtx": np.array([
+                [focal_length, 0,            principal_x],
+                [0,            focal_length, principal_y],
+                [0,            0,            1]
+            ], dtype=np.float32),
+            "distortion": np.zeros(5, dtype=np.float32),
+            # will fill in by setup_camera():
+            "img_size": None,
+            "new_mtx":  None,
+            "roi":      None,
+        }
+
+        self.images_dir = (
+            images_dir or os.path.join(os.getcwd(), "images", "test")
+        )
         self.temp_dir = os.path.join(os.getcwd(), "temp")
         os.makedirs(self.temp_dir, exist_ok=True)
 
-        self.photos = [photo for photo in os.listdir(images_dir) if photo.lower().endswith(('.jpg', '.png', '.jpeg'))]
+        self.photos = [
+            photo
+            for photo in os.listdir(images_dir)
+            if photo.lower().endswith(('.jpg', '.png', '.jpeg'))
+        ]
 
         if not self.photos:
             raise ValueError(f"No images found in {self.images_dir}")
-        self.img_size = None
-        self.new_mtx = None
-        self.roi = None
-        self.setup_camera()
+        self._setup_camera()
 
-    def setup_camera(self):
+    def _setup_camera(self):
+        """Set up the camera intrinsics for cropping."""
         first_img_path = os.path.join(self.images_dir, self.photos[0])
         img = cv2.imread(first_img_path)
         h, w = img.shape[:2]
-        self.img_size = (w, h)
-        self.new_mtx, self.roi = cv2.getOptimalNewCameraMatrix(self.mtx, self.distortion_coeff, self.img_size, 1, self.img_size)
+        self.camera["img_size"] = (w, h)
+        new_mtx, roi = cv2.getOptimalNewCameraMatrix(
+            self.camera['mtx'], self.camera["distortion"],
+            self.camera["img_size"], 1, self.camera["img_size"]
+        )
+        self.camera['new_mtx'] = new_mtx
+        self.camera['roi'] = roi
 
     def process_images(self):
-        x, y, w, h = self.roi
+        """Process the aerial photographs."""
+        x, y, w, h = self.camera['roi']
         for i, photo in enumerate(self.photos):
             img_path = os.path.join(self.images_dir, photo)
             img = cv2.imread(img_path)
-            processed_photo = cv2.undistort(img, self.mtx, self.distortion_coeff, None, self.new_mtx)
-            #crop via regin of interest
+            processed_photo = cv2.undistort(
+                img, self.camera['mtx'],
+                self.camera["distortion"], None, self.camera["new_mtx"]
+            )
+            # crop regin of interest
             processed_photo = processed_photo[y:y+h, x:x+w]
-            processed_image_path = os.path.join(self.temp_dir, f"processed_image_{i}.jpg")
+            processed_image_path = os.path.join(
+                self.temp_dir,
+                f"processed_image_{i}.jpg"
+            )
             cv2.imwrite(processed_image_path, processed_photo)
